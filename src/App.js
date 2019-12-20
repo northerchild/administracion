3100c5189eb300decf85a7bc3a53bfb43314fa69
northerchild <osarus-13@hotmail.com>
2019-12-20 13:43:18 -0500

    Se crea la DB

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Productos from './components/Productos';
 import EditarProductos from './components/EditarProducto';
 import AgregarProducto from './components/AgregarProducto';
+import Header from './components/Header';
 function App() {
   return (
     <Router>
+      <Header />
+      <main className="container mt-5">
       <Switch>
-        <Route exact path="/productos/nuevo" component={Productos}/>
+        <Route exact path="/nuevo-producto" component={AgregarProducto}/>
         <Route exact path="/productos" component={Productos}/>
-        <Route exact path="/productos/:id" component={AgregarProducto}/>
+        <Route exact path="/productos/:id" component={Productos}/>
         <Route exact path="/productos/editar/:id" component={EditarProductos}/>
       </Switch>
+      </main>
+      <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
     </Router>
   );
 }
