3507311b5d2c6b59901da2f03bc924b1da8ed853
northerchild <osarus-13@hotmail.com>
2020-01-02 14:41:32 -0500

    Se agrega el formulario para editar los productos

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,8 +1,102 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Error from './Error';
 
 function EditarProducto(){
+    const [error, guardarError] = useState(false);
+    const [ categoria, guardarCategoria ] = useState('');
+    const leerValorRadio = e=>{
+        guardarCategoria(e.target.value);
+    }
+    const editarProducto = e =>{
+        
+    }
     return(
-        <h1 className="text-center">Editar Nuevo Producto</h1>
+        <div className="col-md-8 mx-auto ">
+            <h1 className="text-center">Editar Producto</h1>
+
+            {(error) ? <Error mensaje='Todos los campos son obligatorios' /> : null }
+
+            <form
+                className="mt-5"
+                onSubmit={editarProducto}
+            >
+                <div className="form-group">
+                    <label>Nombre Platillo</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        name="nombre" 
+                        placeholder="Nombre Platillo"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Precio Platillo</label>
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        name="precio"
+                        placeholder="Precio Platillo"
+                    />
+                </div>
+
+                <legend className="text-center">Categoría:</legend>
+                <div className="text-center">
+                <div className="form-check form-check-inline">
+                    <input 
+                        className="form-check-input" 
+                        type="radio" 
+                        name="categoria"
+                        value="postre"
+                        onChange={leerValorRadio}
+                    />
+                    <label className="form-check-label">
+                        Postre
+                    </label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input 
+                        className="form-check-input" 
+                        type="radio" 
+                        name="categoria"
+                        value="bebida"
+                        onChange={leerValorRadio}
+                    />
+                    <label className="form-check-label">
+                        Bebida
+                    </label>
+                </div>
+
+                <div className="form-check form-check-inline">
+                    <input 
+                        className="form-check-input" 
+                        type="radio" 
+                        name="categoria"
+                        value="cortes"
+                        onChange={leerValorRadio}
+                    />
+                    <label className="form-check-label">
+                        Cortes
+                    </label>
+                </div>
+
+                <div className="form-check form-check-inline">
+                    <input 
+                        className="form-check-input" 
+                        type="radio" 
+                        name="categoria"
+                        value="ensalada"
+                        onChange={leerValorRadio}
+                    />
+                    <label className="form-check-label">
+                        Ensalada
+                    </label>
+                </div>
+                </div>
+
+                <input type="submit" className="font-weight-bold text-uppercase mt-5 btn btn-primary btn-block py-3" value="Editar Producto" />
+            </form>
+        </div>
     )
 }
 export default EditarProducto;
