444f1fe1710adf02fd2410c378829c1e8d48816c
northerchild <osarus-13@hotmail.com>
2020-01-07 19:16:48 -0500

    Se puede eliminar registros y se termina el crud

--- src/components/ProductoLista.js
+++ src/components/ProductoLista.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+function ProductoLista({producto,guardarRecargarProductos}) {
+    const eliminarProducto = id => {
+        console.log('eliminando', id);
 
-function ProductoLista({producto}) {
-    const eliminarProducto = id=>{
-        console.log('eliminar el id', id);
+        // TODO: Eliminar los registros
+        Swal.fire({
+            title: '¿Estas Seguro?',
+            text: "Un Platillo eliminado no se puede recuperar",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminar',
+            cancelButtonText : 'Cancelar'
+        }).then( async (result) => {
+            if (result.value) {
+                try {
+                    const url = `http://localhost:4000/restaurant/${id}`;
+                    const resultado = await axios.delete(url);
+                    if(resultado.status === 200){
+                        Swal.fire(
+                            'Eliminado!',
+                            'El Producto se ha eliminado',
+                            'success'
+                        )
+                        // Consultar la api nuevamente
+                        guardarRecargarProductos(true)
+                    }
+                } catch (error) {
+                    console.log(error);
+                    Swal.fire({
+                        type: 'error',
+                        title: 'Error',
+                        text: 'Hubo un error, vuelve a intentarlo'
+                    })
+                }
+            }
+        })
     }
     return(
         <li className="list-group-item d-flex justify-content-between aling-items-center" data-categoria={producto.categoria}>
